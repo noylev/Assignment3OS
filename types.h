67c43a4564df89deb2eff5d5e40a71a18615cb2d
Amir Arbel <amirarbel@gmail.com>
2018-05-26 22:28:33 +0300

    fdsf

--- types.h
+++ types.h
@@ -2,3 +2,11 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 typedef uint pde_t;
+
+// For page statistics tracking.
+struct page_statistics{
+  uint inital_number;
+  uint current_number;
+};
+
+extern struct page_statistics page_statistics;
